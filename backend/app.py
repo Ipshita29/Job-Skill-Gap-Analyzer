4d34bdfe22fbec666eb8990e8b189cb5e61e66a5
Ipshita Patel <ipshita2908@gmail.com>
2026-04-02 17:30:23 +0530

    .

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from utils import extract_text_from_pdf, analyze
 
-app = Flask(__name__, static_folder="../frontend", static_url_path="")
+app = Flask(__name__, static_folder="frontend", static_url_path="")
 CORS(app)
 
 # Serve frontend
